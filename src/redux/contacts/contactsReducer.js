5c5a6b7f32603de0c148da61b4e527eee533ec5a
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-05-16 21:19:09 +0300

    refactored using createAsyncThunk

--- src/redux/contacts/contactsReducer.js
+++ src/redux/contacts/contactsReducer.js
@@ -1,24 +1,22 @@
 import { combineReducers, createReducer } from '@reduxjs/toolkit';
 import * as contactsActions from './contactsActions';
+import { fetchContacts, addContact, removeContact } from './contactsOperations';
 
 const items = createReducer([], {
-  [contactsActions.fetchContactsSuccess]: (_, action) => action.payload,
-  [contactsActions.addContactSuccess]: (state, action) => [
-    ...state,
-    action.payload,
-  ],
-  [contactsActions.removeContactSuccess]: (state, action) => {
+  [fetchContacts.fulfilled]: (_, action) => action.payload,
+  [addContact.fulfilled]: (state, action) => [...state, action.payload],
+  [removeContact.fulfilled]: (state, action) => {
     return state.filter(item => item.id !== action.payload.id);
   },
 });
 
 const error = createReducer(null, {
-  [contactsActions.fetchContactsError]: (_, action) => action.payload,
-  [contactsActions.fetchContactsRequest]: () => null,
-  [contactsActions.addContactError]: (_, action) => action.payload,
-  [contactsActions.addContactRequest]: () => null,
-  [contactsActions.removeContactError]: (_, action) => action.payload,
-  [contactsActions.removeContactRequest]: () => null,
+  [fetchContacts.rejected]: (_, action) => action.payload,
+  [fetchContacts.pending]: () => null,
+  [addContact.rejected]: (_, action) => action.payload,
+  [addContact.pending]: () => null,
+  [removeContact.rejected]: (_, action) => action.payload,
+  [removeContact.pending]: () => null,
 });
 
 export const filterReducer = createReducer('', {
@@ -29,3 +27,14 @@ export default combineReducers({
   items,
   error,
 });
+//=========================================================
+// const items = createReducer([], {
+//   [contactsActions.fetchContactsSuccess]: (_, action) => action.payload,
+//   [contactsActions.addContactSuccess]: (state, action) => [
+//     ...state,
+//     action.payload,
+//   ],
+//   [contactsActions.removeContactSuccess]: (state, action) => {
+//     return state.filter(item => item.id !== action.payload.id);
+//   },
+// });
